bde54d8c40066532d38bbd3077cde61800f59323
Yash Singh <ypsyashpratapsingh@gmail.com>
2025-08-17 10:36:12 +0530

    Two New

--- BrutalMajorityElement.cpp
+++ BrutalMajorityElement.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 int main(){
-    
+    vector<int> nums = {1,1,1,1,1,2,2,2};
+    int si = nums.size();
+    sort(nums.begin(),nums.end());
+    int freq = 1;
+    int ans = nums[0];
+    for (int i = 1 ;i < si ; i++ ){
+        if (nums[i]==nums[i-1]){
+            freq++;
+        }
+        else {
+            freq = 1;
+        }
+        if (freq > si/2){
+            ans = nums[i];
+            cout << ans;
+            break;
+        }
+    }
+
+
 }
