85204bf94b4072deedcf67e5c8f3bda946cf5540
Yash Singh <ypsyashpratapsingh@gmail.com>
2025-08-22 09:32:51 +0530

    Selection Sort

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
 
+void SelectionSort (int arr[],int n){
+    for (int i = 0 ; i < n ; i++ ){
+        int smallest = i;
+        for (int j = i+1 ; j < n ; j++){
+            if (arr[j]<arr[smallest]){
+                smallest = j;
+            }
+        }
+        swap (arr[i],arr[smallest]);
+    }
+}
+
+void printArray(int arr[], int n){
+    for (int i = 0 ; i < n ; i++){
+        cout << arr[i] << " ";
+    }
+}
+
 int main(){
-    
+    int arr[] ={4,7,1,3,0};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    SelectionSort(arr,n);
+    printArray(arr,n);
 }
