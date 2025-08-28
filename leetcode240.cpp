153fda05946bf469e5ab4df5640308f5716cd460
Yash Singh <ypsyashpratapsingh@gmail.com>
2025-08-28 19:43:22 +0530

    Leetcode 240

---
+++ leetcode240.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<vector<int>> matrix = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+    int m = matrix.size();
+    int n = matrix[0].size();
+    int target = 5;
+    int r = 0 ; int c = n-1;
+    while (r < m && c >= 0){
+        if (matrix[r][c]==target){
+            cout << true;
+            return 1;
+        }
+        else if (matrix[r][c]>target){
+            c--;
+        }
+
+        else if (matrix[r][c]<target){
+            r++;
+        }
+    }
+    cout << false;
+    return 0;
+
+}
