e2dba6a6a42b13e5c9e4250391489d937fd77287
Yash Singh <ypsyashpratapsingh@gmail.com>
2025-09-13 11:55:38 +0530

    Leetcode 232

---
+++ implementQueueUsingStacks.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class MyQueue {
+public:
+    stack<int> s1;
+    stack<int> s2;
+    MyQueue() {
+    }
+    
+    void push(int x) {
+        while (!s1.empty()){
+            s2.push(s1.top());
+            s1.pop();
+        }
+        s1.push(x);
+        while (!s2.empty()){
+            s1.push(s2.top());
+            s2.pop();
+        }    
+    }
+    
+    int pop() {
+        int ans = s1.top();
+        s1.pop();
+        return ans;
+    }
+    
+    int peek() {
+        return s1.top();
+    }
+    
+    bool empty() {
+        return s1.empty();
+    }
+};
+
+int main() {
+    MyQueue* obj = new MyQueue();
+    obj->push(1);
+    obj->push(2);
+    cout << obj->peek() << endl;
+    cout << obj->pop() << endl;
+    cout << obj->empty() << endl;
+    return 0;
+}
