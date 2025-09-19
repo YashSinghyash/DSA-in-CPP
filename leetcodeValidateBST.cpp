8487911fb0b082d0e6af85f7243aab7eb9dee8f2
Yash Singh <ypsyashpratapsingh@gmail.com>
2025-09-19 10:17:53 +0530

    Leetcode 98

---
+++ leetcodeValidateBST.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+  
+
+class Solution {
+public:
+    bool isHelper(TreeNode* root , long long min , long long max){
+        if (root == NULL) return true;
+        if (root -> val > min && root->val < max){
+            bool left = isHelper(root->left , min , root->val);
+            bool right = isHelper(root->right , root->val , max);
+            return left && right;
+        }
+
+        return false;
+    }
+    bool isValidBST(TreeNode* root) {
+        return isHelper(root , LONG_MIN , LONG_MAX);
+    }
+};
